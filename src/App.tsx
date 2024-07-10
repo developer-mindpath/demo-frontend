9ce2135cdcec8969476be8984ab757fd994dd55a
lalit <lalit@mindpathtech.com>
2024-07-10 15:29:09 +0530

    app modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,9 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from 'react-router-dom';
-// Import the ProfilePage component
-import SignIn from './pages/signIn/signIn';
-import Profile from './pages/profile/profile';
+
 import 'App.css';
-import SignUp from './pages/signUp/signUp';
 
 const App: React.FC = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/signIn" replace />} />
-        <Route path="/signIn" element={<SignIn />} />
-        <Route path="/signUp" element={<SignUp />} />
-        {/* <Route path="/landing" element={<LandingPage />} /> */}
-        <Route path="/profile" element={<Profile />} />{' '}
-        {/* Add the ProfilePage route */}
-      </Routes>
-    </Router>
-  );
+  return <>Main page for Vite app</>;
 };
 
 export default App;
